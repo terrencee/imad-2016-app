0280ca2e22ba8ce71acf7106972f338b74f60901
terrencee <nikhsalt@yahoo.co.in>
2016-10-21 00:10:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,8 +6,14 @@ element.innerHTML = 'New value';
 
 //Move the image
 var img = document.getElementById('madi');
+var marginleft = 0
+function moveright(){
+    marginLeft = marginLeft + 10
+    img.style.marginLeft = marginLeft + 'px;'
+    
+}
 img.onclick = function() {
-    var interval = setInterval(moveleft, 100);
+    var interval = setInterval(moveRight, 100);
     img.style.marginLeft = '100px';
     };
 
